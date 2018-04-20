9ebbe51bb7b5aa624cbd8d42a5920ba607055fa0
Deathsteps <lovely_dreamer@126.com>
2018-04-20 12:08:41 +0800

    serve the index.html

--- egg-server/app/router.js
+++ egg-server/app/router.js
@@ -5,6 +5,8 @@
  */
 module.exports = app => {
   const { router, controller } = app;
-  router.get('/', controller.home.index);
+  // router.get('/', controller.home.index);
+  router.redirect('/', '/public/index.html', 302);
+  router.redirect('/index.html', '/public/index.html', 302);
   router.get('/api/news', controller.news.list);
 };
